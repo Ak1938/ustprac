b2db747745da0a38843d38447dbcb05df465cef1
Ak1938 <295883@ust.com>
2025-04-26 09:07:37 +0530

    initial commit

---
+++ src/main/java/com/ust/PrintNum2ToWord.java
@@ -0,0 +1,40 @@
+package com.ust;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class PrintNum2ToWord {
+	
+public static void main(String[] args) {
+	Logger logger = Logger.getLogger(PrintNum2ToWord.class.getName());
+	logger.info("application started");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter  num");
+	int a = sc.nextInt();
+	logger.info("num is "+a);
+	switch(a)
+	{
+	case 1:
+		System.out.println("one");
+		break;
+	case 2:
+		System.out.println("two");
+		break;
+	case 3:
+		System.out.println("three");
+		break;
+	case 4:
+		System.out.println("four");
+		break;
+	case 5:
+		System.out.println("five");
+		break;
+	case 6:
+		System.out.println("six");
+		break;
+	default:
+		System.out.println("invalid");
+	}	
+}
+}
+
